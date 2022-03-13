b635d6fa216b2a1e11f8c21f8dfb3f2ae1724074
jolly26 <pc4jolly@gmail.com>
2022-03-13 23:59:40 +0000

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,12 @@
  * * main - print if the number is postive, zero, or negative
  * *
  * * Description: using the main function
- * * this program prints "Programming is positive, zero, or negative
+ * * this program prints a program that prints all single digit numbers of base 10 starting from 0
  * * Return: 0
  */
 int main(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
