bcdf3c620aa78e526bc40e632a1a5502969a9902
jolly26 <pc4jolly@gmail.com>
2022-03-13 21:00:35 +0000

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - print a string using a function printf
  *
  * Description: using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art,"
+ * prints "with proper grammar, but the outcome is a piece of art,"
  * not allowed to use the function puts
  * return 0
  */
