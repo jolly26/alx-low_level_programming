54894b703791f2dad542297e1ade04701073cb7b
jolly26 <pc4jolly@gmail.com>
2022-03-14 00:12:00 +0000

    program that prints all the numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * * main - print if the number is postive, zero, or negative
+ * *
+ * * Description: using the main function
+ * * a program that prints all the numbers of base 16 in lowercase
+ * * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
