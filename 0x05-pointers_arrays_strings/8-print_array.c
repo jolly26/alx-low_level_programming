95293203781eb95b9cbd8a410e99413b3aa606f7
jolly26 <pc4jolly@gmail.com>
2022-03-22 23:33:37 +0000

    function that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int *a;
+	int n;
+
+	*a = &n;
+	_putchar(n);
+	printf(&n);
+	return (0);
+}
