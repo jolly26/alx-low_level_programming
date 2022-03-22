cb434fa56925e3318616d7166d6190a4ccd1c0ea
jolly26 <pc4jolly@gmail.com>
2022-03-22 20:56:01 +0000

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,13 +8,6 @@
  */
 void reset_to_98(int *n)
 {
-    int n;
-    n = 402;
+   *n=98;
 
-    int *p;
-    *p=&n;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
 }
