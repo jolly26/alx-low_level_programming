7162f19083f35c94a45f2b60c5953b1429314c0b
jolly26 <pc4jolly@gmail.com>
2022-03-22 23:01:21 +0000

    My updated commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * puts_half - function that prints half of a string, followed by a new line.
  * @str: declaration of n and paramters for the function
@@ -14,19 +14,19 @@ void puts_half(char *str)
 	}
 	if (c % 2 == 0)
 	{
-	for ((a = c / 2); str[a] != 0; a++)
-	{
-		l = str[a];
-		_putchar(l);
-	}
+		for ((a = c / 2); str[a] != 0; a++)
+		{
+			l = str[a];
+			_putchar(l);
+		}
 	}
 	else
-       {
-       for ((a = (c - 1) / 2); str[a] != 0; a++)
-       {
-       l = str[a];
-       _putchar(l);
-       }
-       }
-       _putchar('\n');
+        {
+                for ((a = (c - 1) / 2); str[a] != 0; a++)
+                {
+                        l = str[a];
+                        _putchar(l);
+                }
+        }
+	_putchar('\n');
 }
