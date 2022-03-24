d7df99d9b6e81afd21e7cc8f59a9b921838a7617
jolly26 <pc4jolly@gmail.com>
2022-03-24 20:25:06 +0000

    commit

--- 0x06-pointers_arrays_strings/1-strcat.c
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -1,23 +1,26 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
+
 /**
- * _strncat - function that concatenates two strings
- * @dest: parmater for dest to append to
- * @src: parameter for src
- * @n: parameter for n
+ * main - check the code
+ *
  * Return: Always 0.
  */
-char *_strncat(char *dest, char *src, int n)
+int main(void)
 {
-	int a = 0;
-	int b = 0;
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-	while (dest[a] != '\0')
-		a++;
-	while (src[b] != '\0' && b < n)
-	{
-		dest[a] = src[b];
-		b++;
-		a++;
-	}
-	return (dest);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
