7a705ea9b2a32ba3aa181ecb2f3d560876a2c241
root <pc4jolly@gmail.com>
2022-03-29 20:11:21 +0000

    a function that prints the chessboard

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_memset - fills memory with a constant byte
  *
@@ -13,9 +14,9 @@ char *_memset(char *s, char b, unsigned int n)
 
 {
         unsigned int =i;
-        for (i=0 ;i<n ;i++)
+        for (i=0; i<n; i++)
         {
-        s[i] =b;
+        *(s+i) =b;
         }
         return(s) ;
 }
